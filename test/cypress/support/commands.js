13e494d7376c90ee08e22e9f7538c6e6d8350214
patriciaSR <patricia.suarez.psr@gmail.com>
2020-04-28 18:25:12 +0200

    add delay cypress commands

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -1,13 +1,13 @@
 // Delay Cypress commands
-// const COMMAND_DELAY = 500;
+const COMMAND_DELAY = 500;
 
-// for (const command of ['visit', 'click', 'get', 'type', 'clear', 'reload', 'contains']) {
-//     Cypress.Commands.overwrite(command, (originalFn, ...args) => {
-//         const origVal = originalFn(...args);
-//         return new Promise((resolve) => {
-//             setTimeout(() => {
-//                 resolve(origVal);
-//             }, COMMAND_DELAY);
-//         });
-//     });
-// } 
+for (const command of ['visit', 'click', 'get', 'type', 'clear', 'reload', 'contains']) {
+    Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+        const origVal = originalFn(...args);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(origVal);
+            }, COMMAND_DELAY);
+        });
+    });
+} 
